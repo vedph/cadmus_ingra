b7b8c208e56def907cc6055107c0b552237c2de5
Myrmex <dfusi@hotmail.com>
2021-03-26 14:29:04 +0100

    Adding GraffitiInfoPartSeeder test

---
+++ Cadmus.Seed.Ingra.Parts.Test/GraffitiInfoPartSeederTest.cs
@@ -0,0 +1,51 @@
+﻿using Cadmus.Core;
+using Cadmus.Ingra.Parts;
+using Fusi.Tools.Config;
+using System;
+using System.Reflection;
+using Xunit;
+
+namespace Cadmus.Seed.Ingra.Parts.Test
+{
+    public sealed class GraffitiInfoPartSeederTest
+    {
+        private static readonly PartSeederFactory _factory;
+        private static readonly SeedOptions _seedOptions;
+        private static readonly IItem _item;
+
+        static GraffitiInfoPartSeederTest()
+        {
+            _factory = TestHelper.GetFactory();
+            _seedOptions = _factory.GetSeedOptions();
+            _item = _factory.GetItemSeeder().GetItem(1, "facet");
+        }
+
+        [Fact]
+        public void TypeHasTagAttribute()
+        {
+            Type t = typeof(GraffitiInfoPartSeeder);
+            TagAttribute attr = t.GetTypeInfo().GetCustomAttribute<TagAttribute>();
+            Assert.NotNull(attr);
+            Assert.Equal("seed.it.vedph.ingra.graffiti-info", attr.Tag);
+        }
+
+        [Fact]
+        public void Seed_Ok()
+        {
+            GraffitiInfoPartSeeder seeder = new GraffitiInfoPartSeeder();
+            seeder.SetSeedOptions(_seedOptions);
+
+            IPart part = seeder.GetPart(_item, null, _factory);
+
+            Assert.NotNull(part);
+
+            GraffitiInfoPart p = part as GraffitiInfoPart;
+            Assert.NotNull(p);
+
+            TestHelper.AssertPartMetadata(p);
+
+            // TODO: assert properties like:
+            // Assert.NotEmpty(p.Works);
+        }
+    }
+}
