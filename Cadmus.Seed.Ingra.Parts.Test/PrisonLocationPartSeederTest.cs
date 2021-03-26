20cb70317a8cccc37625df4a103d238d9686f1c2
Myrmex <dfusi@hotmail.com>
2021-03-26 23:15:42 +0100

    PrisonLocationPart test

---
+++ Cadmus.Seed.Ingra.Parts.Test/PrisonLocationPartSeederTest.cs
@@ -0,0 +1,51 @@
+﻿using Cadmus.Core;
+using Cadmus.Ingra.Parts;
+using Fusi.Tools.Config;
+using System;
+using System.Reflection;
+using Xunit;
+
+namespace Cadmus.Seed.Ingra.Parts.Test
+{
+    public sealed class PrisonLocationPartSeederTest
+    {
+        private static readonly PartSeederFactory _factory;
+        private static readonly SeedOptions _seedOptions;
+        private static readonly IItem _item;
+
+        static PrisonLocationPartSeederTest()
+        {
+            _factory = TestHelper.GetFactory();
+            _seedOptions = _factory.GetSeedOptions();
+            _item = _factory.GetItemSeeder().GetItem(1, "facet");
+        }
+
+        [Fact]
+        public void TypeHasTagAttribute()
+        {
+            Type t = typeof(PrisonLocationPartSeeder);
+            TagAttribute attr = t.GetTypeInfo().GetCustomAttribute<TagAttribute>();
+            Assert.NotNull(attr);
+            Assert.Equal("seed.it.vedph.ingra.prison-location", attr.Tag);
+        }
+
+        [Fact]
+        public void Seed_Ok()
+        {
+            PrisonLocationPartSeeder seeder = new PrisonLocationPartSeeder();
+            seeder.SetSeedOptions(_seedOptions);
+
+            IPart part = seeder.GetPart(_item, null, _factory);
+
+            Assert.NotNull(part);
+
+            PrisonLocationPart p = part as PrisonLocationPart;
+            Assert.NotNull(p);
+
+            TestHelper.AssertPartMetadata(p);
+
+            // TODO: assert properties like:
+            // Assert.NotEmpty(p.Works);
+        }
+    }
+}
---
+++ Cadmus.Seed.Ingra.Parts/PrisonLocationPartSeeder.cs
@@ -0,0 +1,43 @@
+﻿using Bogus;
+using Cadmus.Core;
+using Cadmus.Ingra.Parts;
+using Fusi.Tools.Config;
+using System;
+
+namespace Cadmus.Seed.Ingra.Parts
+{
+    /// <summary>
+    /// Seeder for <see cref="PrisonLocation"/> part.
+    /// Tag: <c>seed.it.vedph.ingra.prison-location</c>.
+    /// </summary>
+    /// <seealso cref="PartSeederBase" />
+    [Tag("seed.it.vedph.ingra.prison-location")]
+    public sealed class PrisonLocationPartSeeder : PartSeederBase
+    {
+        /// <summary>
+        /// Creates and seeds a new part.
+        /// </summary>
+        /// <param name="item">The item this part should belong to.</param>
+        /// <param name="roleId">The optional part role ID.</param>
+        /// <param name="factory">The part seeder factory. This is used
+        /// for layer parts, which need to seed a set of fragments.</param>
+        /// <returns>A new part.</returns>
+        /// <exception cref="ArgumentNullException">item or factory</exception>
+        public override IPart GetPart(IItem item, string roleId,
+            PartSeederFactory factory)
+        {
+            if (item == null)
+                throw new ArgumentNullException(nameof(item));
+
+            PrisonLocationPart part = new Faker<PrisonLocationPart>()
+                .RuleFor(p => p.PrisonId, f => f.Lorem.Word().ToLowerInvariant())
+                .RuleFor(p => p.Cell, f => $"{f.Random.Number(1, 30)}")
+                .RuleFor(p => p.Location,
+                    f => $"{f.PickRandom('A','B','C','D','E')}{f.Random.Number(1, 10)}")
+                .Generate();
+            SetPartMetadata(part, roleId, item);
+
+            return part;
+        }
+    }
+}
